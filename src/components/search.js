bcb70363c07db2680f1b1a29451e0312f0e8676a
shreyansh223 <shreyansh.gupta0440@gmail.com>
2023-07-22 17:26:11 +0530

    changed dark-mode button to switch

--- src/components/search.js
+++ src/components/search.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { MdSearch } from 'react-icons/md';
 
-const Search = ({ handleSearchNote }) => {
+const Search = () => {
   return (
     <div className="search">
       <MdSearch className="search-icon" size="1.5em" />
